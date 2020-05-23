ad639a57dac4bb2cf4a882c49d1af24abee53228
Ryan Yang <ryanyang.nz@gmail.com>
2020-05-23 15:49:08 +1200

    set default password.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -7,7 +7,7 @@ import { login } from '../../actions/auth';
 const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: 'ryan@test.com',
-    password: ''
+    password: 'Pa$$w0rd'
   });
 
   const { email, password } = formData;
