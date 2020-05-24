724361dd24d589d7b201bce311a411bea56662ce
Ryan Yang <ryanyang.nz@gmail.com>
2020-05-24 23:31:00 +1200

    Remive github profile.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,7 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
-import ProfileGithub from './ProfileGithub';
+// import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -71,9 +71,9 @@ const Profile = ({
               )}
             </div>
 
-            {profile.githubusername && (
+            {/* {profile.githubusername && (
               <ProfileGithub username={profile.githubusername} />
-            )}
+            )} */}
           </div>
         </Fragment>
       )}
